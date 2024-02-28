69845b81d3a37ca1f24f291762489fba7a007a12
Anurag Kejriwal <anuragkejriwal1@gmail.com>
2024-02-28 15:13:20 -0500

    added cds cell test code

---
+++ cdsCellTest.cpp
@@ -0,0 +1,14 @@
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHIO.h>
+#include <FEHLCD.h>
+#include <FEHServo.h>
+
+int main
+{
+    AnalogInputPin light(FEHIO::P2_7);
+    while (1)
+    {
+        LCD.Write(light.Value());
+    }
+}
