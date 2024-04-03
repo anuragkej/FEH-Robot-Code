2dd3aedc89e8e737826b16c80abc2db2dbbaaed5
Anurag Kejriwal <anuragkejriwal1@gmail.com>
2024-04-03 14:29:32 -0400

    edit

--- pc5.cpp
+++ pc5.cpp
@@ -67,13 +67,9 @@ int main()
 
     driveForward(0.9, 100);
 
-    turn('R', 0.66, 100, -100);
+    turn('R', 0.7, 100, -100);
 
-    driveForward(2.6, 100);
-
-    turn('R', 0.1, 100, -100);
-
-    driveForward(0.25, 100);
+    driveForward(2.7, 100);
 
     // turn('R', 0.7, 80, 25);
     // driveForward(0.3, 100);
